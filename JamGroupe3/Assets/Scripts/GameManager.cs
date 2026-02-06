c0ee3f17f9369b2567fe949a3782eb714719d061
Lucas <Lucas.kominiarz@laposte.net>
2026-02-06 16:04:06 -0500

    Auto stash before merge of "Lucas" and "origin/Ui"

--- JamGroupe3/Assets/Scripts/GameManager.cs
+++ JamGroupe3/Assets/Scripts/GameManager.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager INSTANCE;
-    
-    [SerializeField] private RessourcesScriptableHandler ressources;
-    [SerializeField] private Transform[] spawnersPositions;
-    
+  
     private float _actualMoney;
-    private int[] clickerLevels;
+    
 
     
     private void Awake()
@@ -22,16 +19,6 @@ public class GameManager : MonoBehaviour
         else
         {
             Destroy(gameObject);
-        }
-        clickerLevels = new int[ressources.RessourcesScriptablesTab.Length];
-        for (int i = 0; i < clickerLevels.Length; i++)
-        {
-            clickerLevels[i] = 1;
-        }
-
-        if (spawnersPositions.Length < ressources.RessourcesScriptablesTab.Length)
-        {
-            throw new Exception("You need to assign more spawners in the 'spawners positions' variables");
         }
     }
     
